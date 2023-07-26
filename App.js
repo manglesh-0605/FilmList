91846ebbb3b86a6709bb4c2d82c99a90c0e963c5
Manglesh Tiwari <manglesh0605@gmail.com>
2023-07-26 16:18:07 +0530

    added zoom feature

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import 'react-native-gesture-handler'
 import { ApolloProvider } from '@apollo/client';
 import client from './app/apolloClient';
 import FilmList from './app/FIlmList';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
-      <FilmList />
-    </ApolloProvider>
+
+    <GestureHandlerRootView>
+      <ApolloProvider client={client}>
+        <FilmList />
+      </ApolloProvider>
+    </GestureHandlerRootView>
   );
 }
--- app/FilmItem.js
+++ app/FilmItem.js
@@ -1,47 +1,115 @@
-import { Text, View, Image, useWindowDimensions } from 'react-native'
-import React from 'react'
+import { Text, View, Image, useWindowDimensions, Animated } from 'react-native'
+import React, { useRef, useState, createRef } from 'react'
+import { PanGestureHandler, PinchGestureHandler, State } from 'react-native-gesture-handler';
 
 const FilmItem = ({ item, index }) => {
     const { width } = useWindowDimensions();
+    const scale = useRef(new Animated.Value(1)).current;
+    const translateX = useRef(new Animated.Value(0)).current;
+    const [panEnabled, setPanEnabled] = useState(false);
+    const pinchRef = createRef();
+    const panRef = createRef();
+    const handlePinch = Animated.event([
+        {
+            nativeEvent: { scale }
+        }], {
+        listener: (e) => console.log(e.nativeEvent),
+        useNativeDriver: true
+    })
+
+    const handlePan = Animated.event([
+        {
+            nativeEvent: {
+                translationX: translateX
+            }
+        }], {
+        listener: (e) => console.log(e.nativeEvent),
+        useNativeDriver: true
+    })
+
+    const handlePinchStateChange = ({ nativeEvent }) => {
+        // enabled pan only after pinch-zoom
+        if (nativeEvent.state === State.ACTIVE) {
+            setPanEnabled(true);
+        }
+
+        // when scale < 1, reset scale back to original (1)
+        const nScale = nativeEvent.scale;
+        if (nativeEvent.state === State.END) {
+            if (nScale < 1) {
+                Animated.spring(scale, {
+                    toValue: 1,
+                    useNativeDriver: true
+                }).start();
+                Animated.spring(translateX, {
+                    toValue: 0,
+                    useNativeDriver: true
+                }).start();
+                setPanEnabled(false);
+            }
+        }
+    };
+
+
 
     return (
-        <View style={[{
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.23,
-            shadowRadius: 2.62,
-            elevation: 4,
-            marginBottom: 20,
-            marginTop: index === 0 ? 20 : 0,
-            borderRadius: 10,
-            paddingBottom: 10,
-            backgroundColor: '#fff'
-        }]}>
+        <PanGestureHandler
+            ref={panRef}
+            onGestureEvent={handlePan}
+            simultaneousHandlers={[panRef]}
+            enabled={panEnabled}
+        >
+            <Animated.View>
+                <PinchGestureHandler
+                    ref={pinchRef}
+                    simultaneousHandlers={[pinchRef]}
+                    onGestureEvent={handlePinch}
+                    onHandlerStateChange={handlePinchStateChange}>
+                    <Animated.View
+                        style={[{
+                            shadowColor: "#000",
+                            shadowOffset: {
+                                width: 0,
+                                height: 2,
+                            },
+                            shadowOpacity: 0.23,
+                            shadowRadius: 2.62,
+                            elevation: 4,
+                            marginBottom: 20,
+                            marginTop: index === 0 ? 20 : 0,
+                            borderRadius: 10,
+                            paddingBottom: 10,
+                            backgroundColor: '#fff',
+                        }, {
+                                transform: [
+                                    { scale },
+                                    { translateX }
+                                ]
+                            }]}>
 
-            <Image
-                source={{ uri: 'https://source.unsplash.com/random' }}
-                style={{
-                    width: width - 60,
-                    height: 170,
-                    borderTopLeftRadius: 10,
-                    borderTopRightRadius: 10,
-                    alignSelf: 'center'
-                }}
-            />
+                        <Image
+                            source={{ uri: 'https://source.unsplash.com/random' }}
+                            style={{
+                                width: width - 60,
+                                height: 170,
+                                borderTopLeftRadius: 10,
+                                borderTopRightRadius: 10,
+                                alignSelf: 'center'
+                            }}
+                        />
 
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View>
-                    <Text style={{ paddingHorizontal: 20, fontSize: 18, fontWeight: '600', marginTop: 5, opacity: 0.8 }}>{item.title}</Text>
-                    <Text style={{ paddingHorizontal: 20, fontSize: 14, fontWeight: '400', marginB: 5, opacity: 0.8 }}>By: {item.director}</Text>
-                </View>
-                <Text style={{ paddingHorizontal: 20, fontSize: 12 }}>{item.releaseDate}</Text>
-            </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                            <View style={{ flex: 1 }}>
+                                <Text style={{ paddingHorizontal: 20, fontSize: 18, fontWeight: '600', marginTop: 5, opacity: 0.8 }}>{item.title}</Text>
+                                <Text style={{ paddingHorizontal: 20, fontSize: 14, fontWeight: '400', marginB: 5, opacity: 0.8 }}>By: {item.director}</Text>
+                            </View>
+                            <Text style={{ paddingHorizontal: 20, fontSize: 12, textAlign: 'right' }}>{item.releaseDate}</Text>
+                        </View>
 
-        </View>
-
+                    </Animated.View>
+                </PinchGestureHandler>
+            </Animated.View>
+        </PanGestureHandler>
     )
 }
 
